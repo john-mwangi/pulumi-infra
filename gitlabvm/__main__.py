ac71efee0a92ae0107bae130e74237d84aa7627d
John Mwangi <mail.mwangi@gmail.com>
2024-03-11 23:25:49 +0300

    Update

--- gitlabvm/__main__.py
+++ gitlabvm/__main__.py
@@ -1,31 +1,83 @@
 """Spins a GitLab instance on Pulumi"""
 
-import pulumi
 import pulumi_digitalocean as do
 
-# ref: https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-gitlab-on-ubuntu-20-04
-install_gitlab = '''#!/bin/bash
-sudo ufw allow http
-sudo ufw allow https
-sudo ufw allow OpenSSH
-curl -fsSL https://get.docker.com/ | sh
-docker system prune --all -f
-docker run -d -p 443:443 -p 80:80 -p 23:22 --name gitlab_instance gitlab/gitlab-ce:latest
-'''
+import pulumi
 
-vm = do.Droplet(
-    resource_name="gitlab-server",
-    image="ubuntu-20-04-x64",
-    size="s-2vcpu-4gb",
-    region="ams3",
-    resize_disk=False,
-    user_data=install_gitlab,
-)
 
-reserved_ip = do.ReservedIp("reserved_ip_3", droplet_id=vm.id, region=vm.region)
+def create_droplet(kwargs: dict, user_data: str = None):
+    """Creates a virtual machine on Digital Ocean"""
 
-pulumi.export("vm_ip", vm.ipv4_address)
-pulumi.export("static_ip", reserved_ip.ip_address)
-pulumi.export("monthly_price_usd", vm.price_monthly)
-pulumi.export("disk_size", vm.disk)
-pulumi.export("ram", vm.memory)
+    vm = do.Droplet(
+        **kwargs,
+        resize_disk=False,
+        user_data=user_data,
+    )
+
+    reserved_ip = do.ReservedIp(
+        "reserved_ip",
+        droplet_id=vm.id,
+        region=vm.region,
+    )
+
+    pulumi.export("vm_ip", vm.ipv4_address)
+    pulumi.export("static_ip", reserved_ip.ip_address)
+    pulumi.export("monthly_price_usd", vm.price_monthly)
+    pulumi.export("disk_size", vm.disk)
+    pulumi.export("ram", vm.memory)
+
+
+def resize_droplet(id: str, size: str):
+    """Resizes and existing droplet and retains the IPv4 address"""
+
+    existing_droplet = do.Droplet.get("existing-droplet", id)
+
+    reserved_ip = do.ReservedIpAssignment(
+        "reserved-ip-assign",
+        droplet_id=existing_droplet.id,
+        ip_address=existing_droplet.ipv4_address,
+    )
+
+    new_droplet = do.Droplet(
+        "gitlab-server",
+        image=existing_droplet.image,
+        size=size,
+        region=existing_droplet.region,
+        user_data=existing_droplet.user_data,
+        tags=existing_droplet.tags,
+    )
+
+    pulumi.export("vm_ip", new_droplet.ipv4_address)
+    pulumi.export("static_ip", reserved_ip.ip_address)
+    pulumi.export("monthly_price_usd", new_droplet.price_monthly)
+    pulumi.export("disk_size", new_droplet.disk)
+    pulumi.export("ram", new_droplet.memory)
+
+
+if __name__ == "__main__":
+    import os
+
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    # ref: https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-gitlab-on-ubuntu-20-04
+    install_gitlab = """#!/bin/bash
+    sudo ufw allow http
+    sudo ufw allow https
+    sudo ufw allow OpenSSH
+    curl -fsSL https://get.docker.com/ | sh
+    docker system prune --all -f
+    docker run -d -p 443:443 -p 80:80 -p 23:22 --name gitlab_instance gitlab/gitlab-ce:latest
+    """
+
+    # ref: https://slugs.do-api.dev/
+    kwargs = {
+        "resource_name": "gitlab-server",
+        "size": "s-4vcpu-8gb",
+        "region": "ams3",
+        "image": "ubuntu-20-04-x64",
+    }
+
+    # create_droplet(kwargs, user_data=install_gitlab)
+    resize_droplet(id=os.environ["DROPLET_ID"], size="s-4vcpu-8gb")
