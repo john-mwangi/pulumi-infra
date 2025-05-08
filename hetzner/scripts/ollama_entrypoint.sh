ad9820cc2c63db95b0f0b71dbef2b1f9fe92057f
John Mwangi <mail.mwangi@gmail.com>
2025-05-08 21:55:05 +0300

    Add Ollama entrypoint

---
+++ hetzner/scripts/ollama_entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start Ollama in the background.
+/bin/ollama serve &
+
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "🔴 Retrieve LLAMA3 model..."
+ollama pull qwen3:4b
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
