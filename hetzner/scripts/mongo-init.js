895982f70df19facdb15749ebc4296ee19b78524
John M. <mail.mwangi@gmail.com>
2025-10-30 09:41:46 +0300

    Develop (#22)
    * Update ssh config
    
    Relates to: #13
    
    * Relocate files
    
    * Update readme
    
    Closes #16
    
    * Update configs
    
    * Updates and changes

--- hetzner/scripts/mongo-init.js
+++ hetzner/scripts/mongo-init.js
@@ -1,3 +1,13 @@
+db = db.getSiblingDB('admin');
+
+db.createUser({
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  roles: [
+    { role: 'root', db: 'admin' }
+  ]
+});
+
 db = db.getSiblingDB('llm_browser');
 db.createCollection("prompts");
 db.createCollection("results");
